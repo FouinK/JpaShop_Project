538c48070113bfc8f48fd57dcb328a3187cff7a2
Fouink <yyss9103@gmail.com>
2022-10-06 16:48:43 +0900

    회원가입 테스트 케이스 및 sql 쿼리문 parameter value값 로그 출력 (외부라이브러리 사용가능)

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional              //없으면 트렌잭션 에러남.
+    @Rollback(value = false)     //주석을 제거하게 되면(또는 value = true) 쿼리문이 생기지 않음 이유는 트랜잭션의 특성상 commit을 마치고 query log를 찍어주기 때문에
+    public void testMember() {
+
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertEquals(findMember.getId(), member.getId());
+        Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+        Assertions.assertEquals(findMember, member);
+
+    }
+
+}
